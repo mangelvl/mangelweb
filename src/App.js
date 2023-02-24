8a3869db57805e8db72d6332e2680f6baa12e049
mangelvl <thorristerki@gmail.com>
2023-02-23 22:34:49 -0300

    add typescript and redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 function App() {
   return (
-    <div className='text-gray-500 underline'>
-      hi
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
